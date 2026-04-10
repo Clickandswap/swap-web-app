5a6f3dd370b5b59586df2f0ec54024cab9fae02a
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-04-10 14:49:10 +0100

    Added the tailwind config file to store reusable classnames

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+        colors: {
+        primary: "#fccd29",
+        foundation_blue: "#748D9D",
+        low_blue: "#AFBEC6",
+        secondary: "#517184",
+        accent: "#F59E0B",
+        danger: "#EF4444",
+        dark: "#111111",
+        light: "#ffffff",
+      },
+
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        space_grotesk: ["var(--font-space_grotesk)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
