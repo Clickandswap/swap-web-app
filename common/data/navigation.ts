c198c021102d21d5fe37e93bbcbbcca1a5ef1ca0
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-04-10 14:48:14 +0100

    Added the footer images, word_logo , utils, navigation file

---
+++ common/data/navigation.ts
@@ -0,0 +1,104 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Product",
+    to: "/product",
+  },
+
+  {
+    id: 2,
+    name: "Company",
+    to: "/company",
+  },
+  {
+    id: 3,
+    name: "Developer",
+    to: "/developer",
+  },
+];
+
+export const footerLinks = [
+  {
+    id: 1,
+    name: "PRODUCTS",
+    sublinks: [
+      {
+        name: "Personal",
+        to: "/personal",
+      },
+      {
+        name: "Business",
+        to: "/business",
+      },
+      {
+        name: "Cards",
+        to: "/cards",
+      },
+      {
+        name: "Developers",
+        to: "/developers",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "SOLUTIONS",
+    sublinks: [
+      {
+        name: "Buy crypto",
+        to: "/buy-crypto",
+      },
+      {
+        name: "Sell crypto",
+        to: "/sell-crypto",
+      },
+      {
+        name: "Swap crypto",
+        to: "/swap-crypto",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "RESOURCES",
+    sublinks: [
+      {
+        name: "Blog",
+        to: "/blog",
+      },
+      {
+        name: "FAQs",
+        to: "/faqs",
+      },
+      {
+        name: "Help Center",
+        to: "/help-center",
+      }
+    ],
+  },
+
+  {
+    id: 4,
+    name: "COMPANY",
+    sublinks: [
+      {
+        name: "About",
+        to: "/about",
+      },
+      {
+        name: "Careers",
+        to: "/careers",
+      },
+      {
+        name: "Partners",
+        to: "/partners",
+      },
+      {
+        name: "Investors",
+        to: "/investors",
+      }
+    ]
+  }
+];
