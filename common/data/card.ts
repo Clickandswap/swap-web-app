8273ffdb2a23cb4ec2d7a6f4841674149d86c906
Abdulsamad <lasisiabdulsamad9@gmail.com>
2026-04-12 12:13:14 +0100

    Added the datas needed for card, added images to the public folder

---
+++ common/data/card.ts
@@ -0,0 +1,25 @@
+export const cardDetails = [
+  {
+    id: 1,
+    img_path: "/gif/card_easy_to_use.gif",
+    title: "Easy to Use",
+    details:
+      "Whether you’re a beginner or seasoned pro,Skip the complexity - ClickNSwap lets you buy, withdraw or swap crypto quickly with a clean, no-hassle experience.",
+  },
+
+  {
+    id: 2,
+    img_path: "/gif/card_fast_and_secure.gif",
+    title: "Fast and Secure",
+    details:
+      "Your crypto, your control. Security at every stage.Uncompromising speed. Optimal performance, all the time.",
+  },
+
+  {
+    id: 3,
+    img_path: "/gif/card_best_rate_always.gif",
+    title: "The best rate always",
+    details:
+      "Access the best rates from top-tier trusted liquidity partners and strong risk management.",
+  },
+];
